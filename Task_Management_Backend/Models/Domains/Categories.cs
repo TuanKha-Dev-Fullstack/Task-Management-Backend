03dd2c1e726eab655f8814b7b1ebdf4eb4ea4d84
TuanKha-Dev-Fullstack <tuankha.dev@gmail.com>
2024-06-28 21:35:17 +0700

    fix: naming warning and syntax error

--- Task_Management_Backend/Models/Domains/Categories.cs
+++ Task_Management_Backend/Models/Domains/Categories.cs
@@ -1,9 +1,11 @@
-﻿namespace Task_Management_Backend.Models.Domains;
+﻿using System.ComponentModel.DataAnnotations;
 
-public class Categories
+namespace Task_Management_Backend.Models.Domains;
+
+public abstract class Categories
 {
-    [key]
-    public int id { get; set; }
-    public string name { get; set; }
-    public List<TasksInCategory>? tasks_in_category { get; set; }
+    [Key]
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public List<TasksInCategory>? TasksInCategory { get; set; }
 }
