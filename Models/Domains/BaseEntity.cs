34ce02cb1f89a11dbf1785f933d41fe8c294d4df
Lê Tuấn Kha <tuankha.dev@gmail.com>
2024-07-01 20:27:45 +0700

    #9 feat: migration database (#10)

---
+++ Models/Domains/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Task_Management_Backend.Models.Domains;
+
+public abstract class BaseEntity
+{
+    [Key]
+    public int Id { get; init; }
+
+    [Required(ErrorMessage = "Name field is required")]
+    [MaxLength(255, ErrorMessage = "Name field cannot exceed 255 characters")]
+    public required string Name { get; set; }
+    public DateTime Created { get; set; } = DateTime.Now;
+}
--- Models/Domains/Task.cs
+++ Models/Domains/Task.cs
@@ -1,13 +1,8 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Task_Management_Backend.Models.Domains;
-public abstract class Task
+﻿namespace Task_Management_Backend.Models.Domains;
+public class Task : BaseEntity
 {
-    [Key]
-    public int Id { get; set; }
-    public string? Name { get; set; }
-    public DateTime Created { get; set; }
     public bool IsCompeleted { get; set; }
     public bool IsImportant { get; set; }
-    public List<TasksInCategory>? TasksInCategory { get; set; }
+    public int? CategoryId { get; set; }
+    public Categories? Category { get; set; }
 }
