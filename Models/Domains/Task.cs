af240adbbfde765c99b3c511d753bd96dde4b0a1
TuanKha-Dev-Fullstack <tuankha.dev@gmail.com>
2024-07-01 20:05:47 +0700

    refactor: data models

--- Models/Domains/Task.cs
+++ Models/Domains/Task.cs
@@ -1,7 +1,8 @@
 ﻿namespace Task_Management_Backend.Models.Domains;
 public class Task : BaseEntity
 {
-    public bool IsCompeleted { get; set; } = false;
-    public bool IsImportant { get; set; } = false;
-    public Categories? Categories { get; set; }
+    public bool IsCompeleted { get; set; }
+    public bool IsImportant { get; set; }
+    public int? CategoryId { get; set; }
+    public Categories? Category { get; set; }
 }
