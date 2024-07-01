f3e1d2ddb9a4893c546cb5a16c765513b7366369
TuanKha-Dev-Fullstack <tuankha.dev@gmail.com>
2024-07-01 19:43:16 +0700

    feat: validation for data models

--- Models/Domains/BaseEntity.cs
+++ Models/Domains/BaseEntity.cs
@@ -6,6 +6,9 @@ public abstract class BaseEntity
 {
     [Key]
     public int Id { get; init; }
+
+    [Required(ErrorMessage = "Name field is required")]
+    [MaxLength(255, ErrorMessage = "Name field cannot exceed 255 characters")]
     public required string Name { get; set; }
     public DateTime Created { get; set; } = DateTime.Now;
 }
